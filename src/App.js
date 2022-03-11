407f9c928e7ce045129aab6f0bd2b8fbd6929af4
Kim Nickius <mroizo@Kims-MacBook-Air.local>
2022-03-11 12:55:59 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+import Movie from "./components/Movie";
+import "./App.css";
+
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?api_key=7dd175e8e4c6252ca5cf5e63ea198b53&language=en-US&sort_by=popularity.desc&";
+
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?api_key=55f00f23a2d0dbcacd5d1f51e7248eb6&query=";
+//"https://api.themoviedb.org/3/search/movie?&api_key=7dd175e8e4c6252ca5cf5e63ea198b53&query=";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
+
+  const getMovies = (API) => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        //console.log(data);
+        setMovies(data.results);
+      });
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      getMovies(SEARCH_API + searchTerm);
+
+      setSearchTerm("");
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className="search"
+            type="search"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
       </header>
+      <div className="movie-container">
+        {movies.length > 0 &&
+          movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+      </div>
     </div>
   );
 }
---
+++ src/components/Movie.js
@@ -0,0 +1,44 @@
+import { div } from "prelude-ls";
+import React from "react";
+
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+//"https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=7dd175e8e4c6252ca5cf5e63ea198b53&language=en-US";
+
+const setVoteClass = (vote) => {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 6) {
+    return "orange";
+  } else {
+    return "red";
+  }
+};
+
+const Movie = ({ title, poster_path, overview, vote_average }) => (
+  <div className="movie">
+    <img
+      src={
+        poster_path
+          ? IMG_API + poster_path
+          : "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1459&q=80"
+      }
+      alt={title}
+    />
+    <div className="movie-info">
+      <h3>{title}</h3>
+      <span
+        className={`tag ${setVoteClass(vote_average)}
+      `}
+      >
+        {vote_average}
+      </span>
+    </div>
+
+    <div className="movie-over">
+      <h2>Overview</h2>
+      <p>{overview}</p>
+    </div>
+  </div>
+);
+
+export default Movie;
