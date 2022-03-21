060e8774087ebd03dbb75c31b3ed026682253bb2
Kim Nickius <mroizo@Kims-MacBook-Air.local>
2022-03-21 14:39:43 +0100

    new changes

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,9 +5,15 @@ const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
 const setVoteClass = (vote) => {
   if (vote >= 8) {
+    return "gold";
+  } else if (vote >= 7) {
     return "green";
   } else if (vote >= 6) {
+    return "lightgreen";
+  } else if (vote >= 5) {
     return "orange";
+  } else if (vote >= 4) {
+    return "grape";
   } else {
     return "red";
   }
@@ -35,8 +41,11 @@ const Movie = ({ title, poster_path, overview, vote_average }) => {
       </div>
 
       <div className="movie-over">
-        <h2>Overview</h2>
+        <h2>{title}</h2>
         <p>{overview}</p>
+        <span className={`tag ${setVoteClass(vote_average)}`}>
+          {vote_average}
+        </span>
       </div>
     </div>
   );
