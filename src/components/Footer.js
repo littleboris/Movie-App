261d65a6998406e57c27b1b2b11082d1ccaba4e6
Benjamin Sahlgren <bensahlgren@gmail.com>
2022-04-05 21:27:01 +0200

    fixed footer-styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import * as MdIcons from "react-icons/md";
 const Footer = () => {
   return (
     <footer className="main-footer">
-      <div className="container">
+      <div className="footer-container">
         <div className="row">
           {/* Colum1 */}
           <div className="col1">
